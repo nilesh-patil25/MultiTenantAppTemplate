e97005636985464f05af469b82cc0c56e770e15d
Nilesh Patil <nilesh.patil74136@gmail.com>
2024-04-30 16:24:49 +0530

    added tenant model

---
+++ MultiTenantAppTemplate.Server/Models/Tenant.cs
@@ -0,0 +1,19 @@
+﻿namespace MultiTenantAppTemplate.Server.Models
+{
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string Host { get; set; }
+        public bool IsActive { get; set; }
+        public string ThemeName { get; set; }
+
+        public Tenant(int id, string host, bool isActive, string themeName)
+        {
+            Id = id;
+            Host = host;
+            IsActive = isActive;
+            ThemeName = themeName;
+        }
+        public Tenant() { }
+    }
+}
