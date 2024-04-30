91d2b9f1a86860de1ce57830b4266ddf7d89cc52
Nilesh Patil <nilesh.patil74136@gmail.com>
2024-04-30 17:38:06 +0530

    added download image facility according to hostname

--- MultiTenantAppTemplate.Server/Controllers/ImageController.cs
+++ MultiTenantAppTemplate.Server/Controllers/ImageController.cs
@@ -75,6 +75,108 @@ namespace MultiTenantAppTemplate.Server.Controllers
                 return StatusCode(500, $"An error occurred while uploading favicon: {ex.Message}");
             }
         }
+        
+        [HttpGet("downloadBannerImage")]
+        public IActionResult DownloadBannerImage(string hostName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(hostName))
+                {
+                    return BadRequest("Tenant name cannot be null or empty.");
+                }
+
+                string imagePath = GetBannerImagePathForTenant(hostName, "banner");
+
+                if (string.IsNullOrEmpty(imagePath) || !System.IO.File.Exists(imagePath))
+                {
+                    return NotFound($"Image file not found for tenant '{hostName}'.");
+                }
+
+                byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+
+
+                string contentType = "image/jpeg"; // Assume JPEG format
+
+
+                return File(imageBytes, contentType, $"{hostName}_banner_image.jpg");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while downloading image: {ex.Message}");
+            }
+        }
+
+        [HttpGet("downloadFaviconImage")]
+        public IActionResult DownloadFaviconImage(string hostName)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(hostName))
+                {
+                    return BadRequest("Tenant name cannot be null or empty.");
+                }
+
+                string imagePath = GetBannerImagePathForTenant(hostName, "favicon");
+
+                if (string.IsNullOrEmpty(imagePath) || !System.IO.File.Exists(imagePath))
+                {
+                    return NotFound($"Image file not found for tenant '{hostName}'.");
+                }
+
+                byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+
+
+                string contentType = "image/jpeg"; // Assume JPEG format
+
+
+                return File(imageBytes, contentType, $"{hostName}_banner_image.jpg");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while downloading image: {ex.Message}");
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+        private string GetBannerImagePathForTenant(string hostName, string imageType)
+        {
+            try
+            {
+                var basePath = _configuration.GetSection("AssetPath").Value;
+                var fileName = "";
+
+                // This is just a placeholder, replace it with your actual logic
+                string imagesFolder = Path.Combine(basePath, hostName);
+                if (!Directory.Exists(imagesFolder))
+                {
+                    return string.Empty;
+                }
+                string[] imageFiles = Directory.GetFiles(imagesFolder);
+                if (imageFiles.Length == 0)
+                {
+                    return string.Empty;
+                }
+                foreach (var imageFile in imageFiles)
+                {
+                    if (!string.IsNullOrEmpty(imageFile))
+                    {
+                        if (imageFile.Contains(imageType))
+                        {
+                            return Path.Combine(imagesFolder, imageFile);
+                        }
+                    }
+                }
+                throw new FileNotFoundException("Banner image not found for tenant.");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while getting the image path for tenant");
+            }
+        }
         #endregion
 
     }
