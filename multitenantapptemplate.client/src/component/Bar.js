e1ad25d15a5844433deff1570f5eea69ac17a8e6
Nilesh Patil <nilesh.patil74136@gmail.com>
2024-05-02 13:33:04 +0530

    changes in bar and foo component for getting uploaded images on frontend

---
+++ multitenantapptemplate.client/src/component/Bar.js
@@ -0,0 +1,83 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Bar = () => {
+  const [assetImages, setAssetImages] = useState([]);
+
+  useEffect(() => {
+    fetchAssetImages();
+  }, []);
+
+  const fetchAssetImages = async () => {
+    try {
+      const response = await axios.get("/assets/bar");
+      setAssetImages(response.data);
+    } catch (error) {
+      console.error("Error fetching asset images:", error);
+    }
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    uploadImage(file);
+  };
+
+  const uploadImage = async (file) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      await axios.post(
+        "https://localhost:7213/api/Image/bar/banner",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      alert("Image uploaded successfully!");
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      alert("Error uploading image. Please try again.");
+    }
+  };
+
+  const handleButtonClick = () => {
+    const fileInput = document.getElementById("file-input");
+    fileInput.click();
+  };
+
+  return (
+    <>
+      <div style={{ backgroundColor: "#FFE5CC" }}>
+        <h1>Tenant || Bar</h1>
+        <button onClick={handleButtonClick}>Upload Image</button>
+        <input
+          id="file-input"
+          type="file"
+          onChange={handleFileChange}
+          style={{ display: "none" }}
+        />
+        {""}
+        {/* <button onClick={fetchImageData}>Download Image</button>
+        {imageData && <img src={imageData} alt="Banner" />} */}
+
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {assetImages.map((image, index) => (
+            <img
+              key={index}
+              style={{ width: "200px", height: "200px", margin: "12px" }}
+              src={`assets/bar/${image}`}
+              alt={`Image ${index}`}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Bar;
